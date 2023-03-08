cddc6000798f2a7f6eaf1c249e4b018d2279dbe4
Work Git <rborrell@ptc.com>
2023-03-07 23:13:35 -0500

    feat: added contactform with dependencies - rough UI but in working order

--- GGC-site/src/components/Social.jsx
+++ GGC-site/src/components/Social.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from '../style'
+import ContactForm from './ContactForm'
 import Facebook from './Facebook'
 import Maps from './Maps'
 
@@ -14,6 +15,16 @@ const Social = () => {
             Connect on
             <span className='facebook_blue'> Facebook!</span>
             <Facebook/>
+            </h1>
+        </div>
+      </div>
+
+        <div>
+        <div className='sm:float-left float-center'>
+            <h1 className='flex-1 font-poppins font-semibold ss:text-[28px] text-[20px] text-white ss:leading-[100.8px] leading-[75px]'>
+            Send Us An
+            <span className='facebook_blue'> Email!</span>
+            <ContactForm/>
             </h1>
         </div>
         
