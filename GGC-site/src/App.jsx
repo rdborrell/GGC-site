8d9948c77683da1915cc68238d238615a475ec2a
rdborrell <71460542+rdborrell@users.noreply.github.com>
2023-03-15 11:13:25 -0400

    Revert "Add footer #3"

--- GGC-site/src/App.jsx
+++ GGC-site/src/App.jsx
@@ -1,36 +1,33 @@
-import React from "react";
-import styles from "./style";
+import React from 'react'
+import styles from './style'
 
-import { NavBar, Hero, Footer } from "./components";
+import { NavBar, Hero, Footer} from "./components"
 
 const App = () => {
-    return (
-        <div className="bg-transparent h-screen bg-primary">
-            <div className="w-full overflow-hidden">
-                <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-                    <div className={`${styles.boxWidth}`}>
-                        <NavBar />
-                    </div>
-                </div>
-                
-                <div className={`bg-primary ${styles.flexStart}`}>
-                    <div className={`${styles.boxWidth}`}>
-                        <Hero />
-                    </div>
-                </div>
-                
-                <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-                    <div className={`${styles.boxWidth}`}></div>
-                </div>
-                
-                <div className={`bg-primary`}>
-                    <div className="w-full mt-5 border-t-4 border-[#d39e2c]">
-                        <Footer />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className='bg-transparent'>
+      <div className='w-full overflow-hidden'>
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+          <div className={`${styles.boxWidth}`}>
+            <NavBar />
+          </div>
         </div>
-    );
-};
 
-export default App;
+
+        <div className={`bg-primary ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Hero />
+          </div>
+        </div>
+
+        <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
